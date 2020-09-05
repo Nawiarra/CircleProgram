eca2a256d41c00ed538b14aec35023d6627fa53f
Nawiarra <nika.berezovskaya@ukr.net>
2020-09-05 16:37:19 +0300

    Add program, that I made in auditorium

---
+++ CircleProgram/CircleCore/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PointCore;
+
+namespace CircleCore
+{
+
+    public class Circle
+    {
+        public Point Center { get; private set; }
+
+        private double radius;
+        public double Radius
+        {
+            get { return radius; }
+
+            private set
+            {
+                radius = (value < 0) ? Math.Abs(value) : value;
+            }
+        }
+        public Circle(Point center, double radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        public static bool IsAreaEquals(double areaFirstEl, double areaSecondEl)
+        {
+            if (Math.Abs(areaFirstEl - areaSecondEl) < Double.Epsilon)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ CircleProgram/CircleProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CircleCore;
+using PointCore;
+
+namespace CircleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(new Point(0, 0), 12);
+            Circle c2 = new Circle(new Point(3, 5), 11);
+
+            if (Circle.IsAreaEquals(c1.CalculateArea(), c2.CalculateArea()))
+            {
+                Console.WriteLine("Is equals");
+            }
+            else
+            {
+                Console.WriteLine("Is not equals");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
